f30addc25ce14b1dd0bfdd975a9791f5bd31ca62
lleaff <lleaff@users.noreply.github.com>
2015-06-07 03:45:29 +0200

    +gulp browser-sync: inject css, reload otherwise & ~put file globs in variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
-/* ===Gulp operations */
+/* Command line arguments */
+var argv = require('yargs').argv;
+var production = argv.production || argv.p;
+
+/* =Dependencies
+------------------------------------------------------------ */
+/* Gulp operations
+============= */
 var gulp = require('gulp');
 //var es = require('event-stream');
 //var gutil = require('gulp-util');
 //var merge = require('merge-stream');
 var gulpif = require('gulp-if');
-/* ===Content operations */
+/* Content operations
+============= */
 /* General */
 var sourcemaps = require('gulp-sourcemaps'); /* sourcemaps */
 var concat = require('gulp-concat'); /* sourcemaps */
@@ -16,11 +24,14 @@ var uglify = require('gulp-uglify'); /* sourcemaps */
 var sass = require('gulp-sass'); /* sourcemaps */
 /* css */
 var minifyCss = require('gulp-minify-css'); /* sourcemaps */
+/* Misc
+============= */
+var browserSync = !production ? require('browser-sync').create() : null;
 
 var debug = require('gulp-debug'); /* DEBUG */
 
 /* =Variables
- * ------------------------------------------------------------ */
+------------------------------------------------------------ */
 var package = require('./package.json');
 
 var basePaths = {
@@ -31,9 +42,14 @@ var basePaths = {
 var paths = {
 	js: basePaths.source+'static/',
 	scss: basePaths.source+'static/scss/',
+	css: basePaths.source+'static/css/',
 	html: basePaths.source+'templates/',
 	sourcemaps: '../sourcemaps/',
 };
+var files = {};
+Object.keys(paths).forEach(function(ft) {
+  files[ft] = paths[ft]+'**/*.'+ft;
+});
 
 var outPaths = {
   js: basePaths.build+'static/',
@@ -41,13 +57,8 @@ var outPaths = {
 	html: basePaths.build+'templates/',
 };
 
-/* Command line arguments */
-var argv = require('yargs').argv;
-
-var production = argv.production || argv.p;
-
 /* =Tasks
- * ------------------------------------------------------------ */
+------------------------------------------------------------ */
 
 gulp.task('default', ['build']);
 
@@ -59,31 +70,42 @@ gulp.task('otherFiles', function() {
     'js', 'html', 'css', 'scss', 'md'
   ].map(function(ft) { return '!'+basePaths.source+'**/*.'+ft; });
 
-  gulp.src([basePaths.source+'**/*.*'].concat(excludeTypes))
+  return gulp.src([basePaths.source+'**/*.*'].concat(excludeTypes))
     .pipe(gulp.dest(basePaths.build));
 });
 
 gulp.task('js', function() {
-  gulp.src(paths.js+'/**/*.js')
+  return gulp.src(files.js)
     .pipe(gulp.dest(outPaths.js));
 });
 
 gulp.task('html', function() {
-  gulp.src(paths.html+'/**/*.html')
+  return gulp.src(files.html)
     .pipe(gulp.dest(outPaths.html));
 });
 
 gulp.task('css', function() {
-  gulp.src(paths.scss+'/**/*.scss')
+  return gulp.src([files.scss, files.css])
     .pipe(gulpif(!production, sourcemaps.init()))
       .pipe(sass().on('error', sass.logError))
       .pipe(minifyCss())
     .pipe(gulpif(!production, sourcemaps.write(paths.sourcemaps)))
-    .pipe(gulp.dest(outPaths.css));
+    .pipe(gulp.dest(outPaths.css))
+    .pipe(gulpif(!production, browserSync.stream()));
+});
+
+gulp.task('browser-sync', ['build'], function() {
+  if (production) { return; }
+
+  browserSync.init({
+    proxy: "localhost:8080"
+  });
+  gulp.watch([files.scss, files.css], ['css']);
+  gulp.watch(files.html).on('change', browserSync.reload);
 });
 
 /* Helper functions
- * ------------------------------------------------------------ */
+------------------------------------------------------------ */
 function commentString(string, fileType) {
 	var tags = {
 		html:	['<!--', '-->'],
