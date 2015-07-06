4e0bc7cdcfc8e4cbd403bf9b9d6304598b9682db
lleaff <lleaff@users.noreply.github.com>
2015-07-06 11:50:54 +0200

    Incorporate Placeholders: 2febe58 eba2ebc (hash bans)

---
+++ src/static/hashBans.js
@@ -0,0 +1,72 @@
+var boardUri;
+
+if (!DISABLE_JS) {
+
+  var boardIdentifier = document.getElementById('boardIdentifier');
+
+  if (boardIdentifier) {
+    boardUri = boardIdentifier.value;
+  }
+
+  document.getElementById('createFormButton').style.display = 'none';
+  document.getElementById('createJsButton').style.display = 'inline';
+
+  var hashBansDiv = document.getElementById('hashBansdiv');
+
+  for (var j = 0; j < hashBansDiv.childNodes.length; j++) {
+    processHashBanCell(hashBansDiv.childNodes[j]);
+  }
+}
+
+function processHashBanCell(cell) {
+
+  var button = cell.getElementsByClassName('liftjsButton')[0];
+  button.style.display = 'inline';
+
+  button.onclick = function() {
+    liftHashBan(cell.getElementsByClassName('idIdentifier')[0].value);
+  };
+
+  cell.getElementsByClassName('liftFormButton')[0].style.display = 'none';
+
+}
+
+function liftHashBan(hashBan) {
+  apiRequest('liftHashban', {
+    hashBanId : hashBan
+  }, function requestComplete(status, data) {
+
+    if (status === 'ok') {
+      
+      location.reload(true);
+
+    } else {
+      alert(status + ': ' + JSON.stringify(data));
+    }
+  });
+}
+
+function placeHashBan() {
+
+  var typedHash = document.getElementById('hashField').value.trim();
+
+  var parameters = {
+    hash : typedHash
+  };
+
+  if (boardUri) {
+    parameters.boardUri = boardUri;
+  }
+
+  apiRequest('placeHashBan', parameters,
+            function requestComplete(status, data) {
+
+              if (status === 'ok') {
+
+                location.reload(true);
+
+              } else {
+                alert(status + ': ' + JSON.stringify(data));
+              }
+            });
+}
